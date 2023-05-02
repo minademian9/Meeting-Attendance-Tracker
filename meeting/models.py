e7cb0c8f0fa75092d4b05ffa0fc72545838b66ca
Mina Demian <minademian9@gmail.com>
2023-05-02 15:21:39 +0300

    Update models.py

--- meeting/models.py
+++ meeting/models.py
@@ -5,7 +5,10 @@ from django.db import models
 
 class Attendee(models.Model):
     name = models.CharField(max_length=200)
-    mobile = models.IntegerField(default=0, null=True, blank=True)
+    mobile = models.CharField(max_length=200,
+                              default="0",
+                              null=True,
+                              blank=True)
     email = models.CharField(max_length=200, blank=True)
 
     # pub_date = models.DateTimeField('date published')
@@ -26,3 +29,4 @@ class AttendanceRecord(models.Model):
 
     def __str__(self):
         return self.member.name + " " + str(self.record_date)
+        # return str(self.record_date)
